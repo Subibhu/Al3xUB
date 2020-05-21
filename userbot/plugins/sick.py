b25b73e20206fd2bb7137e63823d8f537838665a
Subibhu <59786652+Subibhu@users.noreply.github.com>
2020-05-21 10:33:09 +0545

    Bug Fixes

--- userbot/plugins/sick.py
+++ userbot/plugins/sick.py
@@ -9,7 +9,7 @@ from telethon.tl.types import InputMessagesFilterDocument
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="srgb (.*)"))
+@borg.on(admin_cmd(pattern="beb (.*)"))
 async def sticklet(event):
     R = random.randint(0,256)
     G = random.randint(0,256)
