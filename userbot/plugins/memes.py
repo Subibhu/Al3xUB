51929cd9461e6cf8c71e23b44f2b9ec8ba65dd93
Subibhu <59786652+Subibhu@users.noreply.github.com>
2020-09-07 14:14:50 +0545

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -594,6 +594,36 @@ SLAP_TEMPLATES = [
     "grabs up a {item} and {hits} {victim} with it.",
     "ties {victim} to a chair and {throws} a {item} at them.",
     "gave a friendly push to help {victim} learn to swim in lava."
+    "{hits} {victim} with a {item}.",
+    "{hits} {victim} in the face with a {item}.",
+    "{hits} {victim} around a bit with a {item}.",
+    "{throws} a {item} at {victim}.",
+    "grabs a {item} and {throws} it at {victim}'s face.",
+    "{hits} a {item} at {victim}.", "{throws} a few {item} at {victim}.",
+    "grabs a {item} and {throws} it in {victim}'s face.",
+    "launches a {item} in {victim}'s general direction.",
+    "sits on {victim}'s face while slamming a {item} {where}.",
+    "starts slapping {victim} silly with a {item}.",
+    "pins {victim} down and repeatedly {hits} them with a {item}.",
+    "grabs up a {item} and {hits} {victim} with it.",
+    "starts slapping {victim} silly with a {item}.",
+    "holds {victim} down and repeatedly {hits} them with a {item}.",
+    "prods {victim} with a {item}.",
+    "picks up a {item} and {hits} {victim} with it.",
+    "ties {victim} to a chair and {throws} a {item} at them.",
+    "{hits} {victim} {where} with a {item}.",
+    "ties {victim} to a pole and whips them {where} with a {item}."
+    "gave a friendly push to help {victim} learn to swim in lava.",
+    "sent {victim} to /dev/null.", "sent {victim} down the memory hole.",
+    "beheaded {victim}.", "threw {victim} off a building.",
+    "replaced all of {victim}'s music with Nickelback.",
+    "spammed {victim}'s email.", "made {victim} a knuckle sandwich.",
+    "slapped {victim} with pure nothing.",
+    "hit {victim} with a small, interstellar spaceship.",
+    "quickscoped {victim}.", "put {victim} in check-mate.",
+    "RSA-encrypted {victim} and deleted the private key.",
+    "put {victim} in the friendzone.",
+    "slaps {victim} with a DMCA takedown request!"
 ]
 
 ITEMS = [
@@ -625,7 +655,14 @@ ITEMS = [
     "beehive",
     "piece of rotten meat",
     "bear",
-    "ton of bricks",
+    "ton of bricks"
+    "cast iron skillet", "large trout", "baseball bat", "cricket bat", "wooden cane", "nail",
+    "printer", "shovel", "pair of trousers", "CRT monitor", "diamond sword", "baguette",
+    "physics textbook", "toaster", "portrait of Richard Stallman", "television", "mau5head",
+    "five ton truck", "roll of duct tape", "book", "laptop", "old television",
+    "sack of rocks", "rainbow trout", "cobblestone block", "lava bucket", "rubber chicken",
+    "spiked bat", "gold block", "fire extinguisher", "heavy rock", "chunk of dirt",
+    "beehive", "piece of rotten meat", "bear", "ton of bricks",
 ]
 
 THROW = [
